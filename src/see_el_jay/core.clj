9cc42e6ba3ba6894028d7f8dd9b94dce66e87ee7
CyberSapiens97 <cybersapiens97@gmail.com>
2019-08-19 01:32:47 -0300

    Creates initial functions

---
+++ src/see_el_jay/core.clj
@@ -0,0 +1,48 @@
+(ns see-el-jay.core
+  (:require [clojure.core.async :refer [<!!]]
+            [clojure.string :as str]
+            [environ.core :refer [env]]
+            [morse.handlers :as h]
+            [morse.polling :as p]
+            [morse.api :as t])
+  (:gen-class))
+
+
+(def token (env :telegram-token))
+
+(h/defhandler handler
+
+  (h/command-fn "start"
+                (fn [{{id :id :as chat} :chat}]
+                  (println "Bot joined new chat: " chat)
+                  (t/send-text token id "Welcome to see-el-jay!")))
+
+  (h/command-fn "help"
+                (fn [{{id :id :as chat} :chat}]
+                  (println "Help was requested in " chat)
+                  (t/send-text token id "Help is on the way")))
+
+  (h/command-fn "run"
+                (fn [{:keys [text chat]}]
+                  (println "Help was requested in " chat)
+                  (t/send-text token (:id chat) (load-string (subs text 5)))))
+  ;; TODO
+  ;; Handle exceptions
+  ;; Handle when get's too long to execute
+  ;; Should be non-blocking
+  ;; Should answer the user that sent the code to run
+
+  (h/message-fn
+   (fn [{{id :id} :chat :as message}]
+     (println "Intercepted message: " message)
+     (t/send-text token id "I don't do a whole lot ... yet."))))
+
+
+(defn -main
+  [& args]
+  (when (str/blank? token)
+    (println "Please provde token in TELEGRAM_TOKEN environment variable!")
+    (System/exit 1))
+
+  (println "Starting the see-el-jay")
+  (<!! (p/start token handler)))
---
+++ test/see_el_jay/core_test.clj
@@ -0,0 +1,7 @@
+(ns see-el-jay.core-test
+  (:require [clojure.test :refer :all]
+    [see-el-jay.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
