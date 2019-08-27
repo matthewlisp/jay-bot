a701499ac6164f08c1bcddf3f68192c6e802b9b7
matthewlisp <matthewlisp@gmail.com>
2019-08-27 16:02:05 -0300

    improves run-code fn

--- src/see_el_jay/core.clj
+++ src/see_el_jay/core.clj
@@ -9,12 +9,12 @@
 
 (def token (env :telegram-token))
 
-
+;; This function get's thread stucked if used with infinite lazy seq's, made a workaround directly inside the telegram handler for run command.
 (defn run-code
   "Run given expression and returns a map containing info about the result"
   [strg]
   (let [code-execution (future (try (load-string (subs strg 5)) (catch Exception e (str "Exception: " (.getMessage e)))))
-        code-result (deref code-execution 100 :timeout)        
+        code-result (deref code-execution 2000 :timeout)
         str-output (pr-str code-result)
         output-size (count str-output)
         result-map {:code-output code-result :str-output str-output :execution-time 0 :issued-code strg}]
@@ -36,11 +36,12 @@
 
   (h/command-fn "run"
                 (fn [{:keys [text chat message_id]}]
-                  (println "Run command issued at " chat)
-                  (t/send-text token (:id chat)
-                               {:reply_to_message_id message_id
-                                :parse_mode "HTML"}
-                               (str "<code>" (:str-output (run-code text)) "</code>"))))
+                  (let [exec-code (future (:str-output (run-code text)))]
+                    (println "Run command issued at " chat)
+                    (t/send-text token (:id chat)
+                                 {:reply_to_message_id message_id
+                                  :parse_mode "HTML"}
+                                 (str "<code>" (deref exec-code 2000 :timeout) "</code>")))))
   
   (h/message-fn
    (fn [{{id :id} :chat :as message}]
@@ -54,15 +55,15 @@
     (println "Please provde token in TELEGRAM_TOKEN environment variable!")
     (System/exit 1))
 
-  (println "Starting the see-el-jay")
+  (println "Starting the see-el-jay bot")
   (<!! (p/start token handler)))
 
 
-;; General ideas:
-;; /learn - Show resources for learning clojure
-;; /snippetify -txt -img - Save snippet into pastebin/gist or carbon
-;; /lib [clojar] code - Fast test a library (that's a hard one)
-;; /reset - Clear memory and restart application
-;; Auto crawl into communities such as clojure Reditt and repost it
-
+;; General ideas
+;; /learn                   : Show resources for learning clojure
+;; /snippetify -txt OR -img : Save snippet into pastebin/gist or carbon
+;; /lib [clojar] [code]     : Fast test a library (that's a hard one)
+;; /reset                   : Clear memory and restart application
+;; /latest -reditt          : Auto crawl into communities such as clojure Reditt and repost it
+;; /events                  : Show upcoming Clojure-related events
 
--- test/see_el_jay/core_test.clj
+++ test/see_el_jay/core_test.clj
@@ -4,6 +4,7 @@
 
 (def error-msg-terminated "Exception: execution terminated. Your code got too long to execute.")
 (def error-msg-output "Exception: output ommited. The output is too big.")
+(def error-msg-exception "\"Exception: clojure.lang.ArityException: Wrong number of args (0) passed to: core/repeat, compiling:(null:1:1)\"")
 
 (deftest test-run-code
   (testing "Regular code"
@@ -11,10 +12,8 @@
     (is (= [2,3,4] (:code-output (run-code "/run (map inc [1 2 3])"))))
     (is (= true (:code-output (run-code "/run (= 1 1)")))))
   (testing "Output is too big"
-    (is (= error-msg-output (:str-output (run-code "/run (take 1000 (repeat \"Hi\"))"))))
-    (is (= error-msg-output (:str-output (run-code "/run (repeat 0)")))))
-  ;;(testing "Code that get's too long to produce output")
-  ;;(testing "Infinite loop")
-  ;;(testing "Snippet too big to be processed") 
+    (is (= error-msg-output (:str-output (run-code "/run (take 1000 (repeat \"Hi\"))")))))
   (testing "recover from exceptions"
-    (is (= "Exception: Wrong number of args (0) passed to: core/repeat" (:str-output (run-code "/run (take 5 (repeat ))"))))))
+    (is (= error-msg-exception (:str-output (run-code "/run (take 5 (repeat ))"))))))
+
+;; Infinite loops and infinite lazy seqs aren't tested in run-code fn because of a workaround made in morse handler for run command
